d2c82ecfc4a82135727aa7b908fbc012d596e3ea
kevinxkang <kevinakng1k1k@gmail.com>
2022-11-11 00:56:35 -0500

    css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +12,42 @@ bakeryData.forEach((item) => {
 function App() {
     // TODO: use useState to create a state variable to hold the state of the cart
     /* add your cart state code here */
+    const [cart, setCart] = useState([]);
+    const [total, setTotal] = useState(0);
+
+    function addToCart(index, price) {
+        setCart((cart) => {
+            let tempCart = [...cart];
+            tempCart[index] = tempCart[index] + 1 || 1;
+            return tempCart;
+        });
+        setTotal(total + price);
+    }
 
     return (
         <div className="App">
-            <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+            <div className="bakery-wrap">
+                <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
-            {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-                <p>Bakery Item {index}</p> // replace with BakeryItem component
-            ))}
+                <div class="bakery-items">
+                    {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+                        <BakeryItem name={item.name} price={item.price} image={item.image} description = {item.description} index={index} onClick={addToCart}/>
+                    ))}
+                </div>
+            </div>
 
-            <div>
+            <div class ="cart">
                 <h2>Cart</h2>
-                {/* TODO: render a list of items in the cart */}
+                {cart.map((item, index) => {
+                    if (item > 0) {
+                        return (
+                            <p>
+                                {item} x {bakeryData[index].name}
+                            </p>
+                        );
+                    }
+                })}
+                <p>Total: ${total.toFixed(2)}</p>
             </div>
         </div>
     );
